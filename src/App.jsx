668a18fdeafcff05e90c3a04a81faf730bca63c4
user <user@usernoMacBook-Air.local>
2024-09-10 10:31:56 +0900

    editablePlayerName

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Player from "./components/Player";
 
 function App() {
   
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player initialName='player1' symbol='X' />
+        <Player initialName='player2' symbol='0' />
+      </ol>
+
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>;
 }
 
 export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Player({initialName,symbol}){
+    const [playerName,setPlayerName] = useState(initialName);
+    const [ isEditing,setIsEditing ] = useState(false);
+
+    
+    function handleEditClick(){
+      // setIsEditing(!isEditing); //isEditing? false: true
+      setIsEditing((editing) => !editing);
+    };
+    
+    function handleChenge(event){
+      // console.log(event);
+      setPlayerName(event.target.value);
+    };
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    // let btnCaption = 'Edit';
+
+    if(isEditing){
+      editablePlayerName=<input type="text" required value={playerName} onChange={handleChenge} />;
+      // btnCaption='Save';
+    }
+
+    return(
+        <li>
+          <span className="player">
+            {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+          </span>
+          <button onClick={handleEditClick} >{isEditing? 'Save': 'Edit'}</button>
+        </li>
+    );
+};
+
+export default Player;
